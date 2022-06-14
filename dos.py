0bf88ba1b9aa6d4876faa2549a55d2917c756f36
Rihan Ahmed <95731297+Rihan444@users.noreply.github.com>
2022-06-14 15:04:23 +0600

    Update dos.py

--- dos.py
+++ dos.py
@@ -4,26 +4,6 @@ import time
 import socket
 import random
 
-color_off="\033[0m"       # Text Reset
-red="\033[0;31m"          # Red
-blue="\033[0;34m"         # Blue
-yellow="\033[0;33m"       # Yellow
-biblue="\033[1;94m"       # Blue
-bipurple="\033[1;95m"     # Purple
-bicyan="\033[1;96m"       # Cyan
-bgreen="\033[1;32m"       # Green0
-on_purple="\033[45m"      # Purple
-icyan="\033[0;96m"        # Cyan
-bred="\033[1;31m"         # Red
-on_igreen="\033[0;102m"   # Green
-igreen="\033[0;92m"       # Green
-upurple="\033[4;35m"      # Purple
-biehite="\033[1;97m"      # White
-byellow="\033[1;33m"      # Yellow
-bcyan="\033[1;36m"        # Cyan
-iblue="\033[0;94m"        # Blue
-ured="\033[4;31m"         # Red
-ired="\033[0;91m"         # Red
 
 
 ired="\033[0;91m"         # Red
@@ -58,6 +38,6 @@ i = 1
 
 while i<=500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000:
 	if i%1 == 0:
-		print(str(i)+str(ired+''' Start send  Package '''+color_off))
+		print(str(i)+str(ired+''' Start send  Package '''))
 		
 	i = i + 1
